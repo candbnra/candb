4c73357f24384b7a83918548c52090cce22328eb
candbnra <160494259+candbnra@users.noreply.github.com>
2024-02-19 19:39:47 +0100

    commit 2

---
+++ src/main/java/net/nra/candb/CandbApplication.java
@@ -0,0 +1,13 @@
+package net.nra.candb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/nra/candb/CandbApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nra.candb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
