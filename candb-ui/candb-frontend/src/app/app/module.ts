843c61e22866a8102893a879cff2f83c151b9836
candbnra <160494259+candbnra@users.noreply.github.com>
2024-02-21 21:22:24 +0100

    update angular project running fine #1

--- candb-ui/candb-frontend/src/app/app/module.ts
+++ candb-ui/candb-frontend/src/app/app/module.ts
@@ -1,7 +1,9 @@
+import { HttpClientModule, provideHttpClient, withFetch } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "../app.component"; 
 import { EmployeeListComponent } from "../employee-list/employee-list.component";
 import { BrowserModule } from "@angular/platform-browser";
+
 
 @NgModule({
     declarations: [
@@ -9,9 +11,10 @@ import { BrowserModule } from "@angular/platform-browser";
         EmployeeListComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        HttpClientModule
     ],
-    providers: [],
+    providers: [provideHttpClient(withFetch())],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
--- candb-ui/candb-frontend/src/app/employee-list/employee-list.component.ts
+++ candb-ui/candb-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,12 +1,30 @@
+
 import { Component } from '@angular/core';
+import { Employee } from '../employee';
+import { CommonModule } from '@angular/common';
+import { EmployeeService } from '../employee.service';
+
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
+  
+  employees: Employee[] = [];
 
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.getEmployees()
+  }
+
+  getEmployees() {
+   this.employeeService.getEmployees().subscribe(data => {
+      this.employees = data;
+    })
+  }
 }
